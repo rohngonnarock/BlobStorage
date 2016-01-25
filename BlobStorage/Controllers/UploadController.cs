10481768f83ec2f39a74a2c996b00fa772a1d975
rahulpd <rahulpd@goldengekko.com>
2016-01-25 19:08:17 +0530

    Upload api working fine with anuglar js

--- BlobStorage/Controllers/UploadController.cs
+++ BlobStorage/Controllers/UploadController.cs
@@ -20,11 +20,11 @@ namespace BlobStorage.Controllers
         }
 
         [HttpPost]
-        public ActionResult UploadReport(IEnumerable<HttpPostedFileBase> files)
+        public ActionResult UploadReport(HttpPostedFileBase files)
         {
             int ReportTypeID = Convert.ToInt32(HttpContext.Request.Form["ReportType"]);
 
-            HttpPostedFileBase uploadedFile = files.FirstOrDefault();
+            HttpPostedFileBase uploadedFile = files;
             try
             {
                 AjaxResult res = new AjaxResult();
@@ -89,6 +89,42 @@ namespace BlobStorage.Controllers
             {
                 return Json(new AjaxResult { isSuccessfull = false, Data = "", ErrorMessage = ex.InnerException.Message });
             }
+        }
+
+        public ContentResult GetSasUrl()
+        {
+            BlobStorageService blobStorage = new BlobStorageService();
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference("ringtones");
+
+            // Create the container if it doesn't already exist.
+            blobContainer.CreateIfNotExists();
+
+
+            return Content(GetContainerSasUri(blobContainer));
+
+        }
+
+        static string GetContainerSasUri(CloudBlobContainer container)
+        {
+            //Set the expiry time and permissions for the container.
+            //In this case no start time is specified, so the shared access signature becomes valid immediately.
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.List;
+
+            //Generate the shared access signature on the container, setting the constraints directly on the signature.
+            string sasContainerToken = container.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
         }
     }
 }
